2603436568ce86c2ddf7bde3d809090c18501a75
Daniel Jay Haskin <djhaskin987@gmail.com>
2021-01-14 17:38:08 -0700

    So it begins

---
+++ src/main/java/me/djhaskin987/gumshoe/App.java
@@ -0,0 +1,13 @@
+package me.djhaskin987.gumshoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
