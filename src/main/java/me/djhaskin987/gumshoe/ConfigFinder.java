3380ff119ba93545ee10b2fbbaeff36d2ef6fe12
Daniel Jay Haskin <djhaskin987@gmail.com>
2021-01-17 12:18:34 -0700

    Add configfinder changes

--- src/main/java/me/djhaskin987/gumshoe/ConfigFinder.java
+++ src/main/java/me/djhaskin987/gumshoe/ConfigFinder.java
@@ -1,5 +1,7 @@
 package me.djhaskin987.gumshoe;
 
+import java.io.InputStream;
+
 public interface ConfigFinder {
     public boolean pathExists(String path);
     public InputStream getInputStream(String path);
